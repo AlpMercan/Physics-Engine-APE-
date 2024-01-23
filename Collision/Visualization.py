00c316aea04f5ad8a4539d9d66e5251d92f40dfb
Alp Mercan <112685013+AlpMercan@users.noreply.github.com>
2024-01-23 17:23:46 +0300

    Update Visualization.py

--- Collision/Visualization.py
+++ Collision/Visualization.py
@@ -1,35 +1,44 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from Engine_2D import PhysicsObject, Vector2D, check_collision, collision_response, AABB
+from Engine_2D import (
+    PhysicsObject,
+    Vector2D,
+    check_collision,
+    collision_response,
+    AABB,
+    check_collision_circle,
+)
 
-obj1 = PhysicsObject(position=Vector2D(-2, 0), velocity=Vector2D(1, 0), mass=2)
-obj1.aabb = AABB(Vector2D(-1.5, -0.5), Vector2D(-1, 0.5))  # Rectangle coordinates
 
-obj2 = PhysicsObject(position=Vector2D(3, 0), velocity=Vector2D(-1, 0), mass=1)
-obj2.aabb = AABB(Vector2D(2, -0.5), Vector2D(2.5, 0.5))  # Rectangle coordinates
+obj1 = PhysicsObject(
+    position=Vector2D(-2, 0), velocity=Vector2D(1, 0), mass=1, radius=0.05
+)
+obj2 = PhysicsObject(
+    position=Vector2D(3, 0), velocity=Vector2D(-1, 0), mass=1, radius=0.05
+)
 
 
-# Create a function to update the plot
 def update_plot(num):
     global obj1, obj2
+
     obj1.update(0.1)
     obj2.update(0.1)
+
+    if check_collision_circle(obj1, obj2):
+        collision_response(obj1, obj2)
 
     ax.clear()
     ax.set_xlim(-3, 4)
     ax.set_ylim(-1, 1)
 
-    # Plot objects as points
     ax.plot([obj1.position.x], [obj1.position.y], "ro", label="Obj1")
     ax.plot([obj2.position.x], [obj2.position.y], "bo", label="Obj2")
 
     ax.legend()
 
 
-# Create a Matplotlib figure and axis
 fig, ax = plt.subplots()
 
-# Set the initial plot properties
 ax.set_xlim(-3, 4)
 ax.set_ylim(-1, 1)
 ax.plot([obj1.position.x], [obj1.position.y], "ro", label="Obj1")
@@ -37,8 +46,15 @@ ax.plot([obj2.position.x], [obj2.position.y], "bo", label="Obj2")
 
 ax.legend()
 
-# Create an animation with shorter interval and more frames
-ani = animation.FuncAnimation(fig, update_plot, frames=100, interval=50, repeat=True)
 
-# Display the animation
+ani = animation.FuncAnimation(fig, update_plot, frames=100, interval=50, repeat=False)
+
+
+ani.save(
+    "C:/Users/alpme/OneDrive/Desktop/Projeler/Physics Engine/Collision/simulation.gif",
+    writer="imagemagick",
+    fps=30,
+)
+
+
 plt.show()
